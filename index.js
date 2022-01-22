f35af82340b07088d65b2fd1e73388c75427686d
Darcy <darcy.lauder@shaw.ca>
2022-01-21 22:38:23 -0700

    partially working, error on nested empty array

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  if (Array.isArray(array[0])) {
+    sumItems(array[0]);
+  }
 
+  if (array.length === 0) return 0;
+  else return array[0] + sumItems(array.splice(1));
+};
+
+console.log(typeof sumItems([[]]));
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([1, []]));
+console.log(sumItems([1, 2, [[3],4], 5, []]));
 module.exports = sumItems;
